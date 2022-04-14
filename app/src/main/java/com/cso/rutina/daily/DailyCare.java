abaa2746a6d01d8ab3f31513b9d9396a3463f495
K-0joo <tyty4420@naver.com>
2022-04-14 15:58:16 +0900

    initial setting - Daily Care

---
+++ app/src/main/java/com/cso/rutina/daily/DailyCare.java
@@ -0,0 +1,16 @@
+package com.cso.rutina.daily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cso.rutina.R;
+
+public class DailyCare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_care);
+    }
+}
