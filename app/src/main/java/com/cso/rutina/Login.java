60eda1f1a4d9e675339f1689445141013adc6023
Hyun-Jiii <hyunjiiiii.0105@gmail.com>
2022-05-26 06:00:35 +0900

    Issue/#1

---
+++ app/src/main/java/com/cso/rutina/Login.java
@@ -0,0 +1,26 @@
+package com.cso.rutina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+    TextView sign;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //회원가입 버튼
+        sign = findViewById(R.id.signin);
+
+        //회원가입 버튼 클릭시, 회원가입 페이지로 이동
+        sign.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Join.class);
+            startActivity(intent);
+        });
+    }
+}
