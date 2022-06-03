6aa7a9a31c1f34fa450c20f7ed7209adcf2f5acb
Hyun-Jiii <hyunjiiiii.0105@gmail.com>
2022-06-03 13:50:46 +0900

    Issue/#4

---
+++ app/src/main/java/com/cso/rutina/Join_Select1.java
@@ -0,0 +1,13 @@
+package com.cso.rutina;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Join_Select1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_select1);
+    }
+}
